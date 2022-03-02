a49f9d8994c57e86465c65d2228213f5f6ec03db
Matt Rothenberg <mattrothenberg@github.com>
2022-03-02 10:35:58 -0500

    feat: add error route

---
+++ pages/api/error.js
@@ -0,0 +1,8 @@
+import { withSentry } from "@sentry/nextjs";
+
+const handler = async (req, res) => {
+  throw new Error("API throw error test");
+  res.status(200).json({ name: "John Doe" });
+};
+
+export default withSentry(handler);
